c3cac6b4de1c7ad637aec77e659a03adf37bc5f8
g-van <g.vandoros1991@gmail.com>
2022-04-12 18:58:22 +0300

    Setup for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const weatherstack = require('./utils/weatherstack')
 const geocode = require('./utils/geocode')
 
 const app = express()
+const port = process.env.PORT || 3000
+
 
 //define paths
 const publicDirectoryPath = path.join(__dirname, "../public")
@@ -131,6 +133,6 @@ app.get('*', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log('server is up on port 3000')
+app.listen(port, () => {
+    console.log('server is up on port ' + port)
 })
